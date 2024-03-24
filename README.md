faf1dcbd8e93135ddb45c78f76a2dd573c74cdaf
Ekaterina Radchenko <60271557+cryingsealpup@users.noreply.github.com>
2024-03-25 00:11:34 +0400

    fix: dragging

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Kanban board with Vue.js
 
 ## References
 - [How to Add Drag and Drop to Your VueJS Project](https://learnvue.co/articles/vue-drag-and-drop)
+- [Build a Drag-and-Drop Trello Board with Vue.js](https://vueschool.io/courses/build-a-drag-and-drop-trello-board)
--- src/components/KanbanBoard.vue
+++ src/components/KanbanBoard.vue
@@ -1,32 +1,32 @@
 <template>
-    <draggable group="track" class="tracks" :list="tracks" item-key="id" @start="drag=true" @end="drag=false" >
-        <template #item="{ element: track }">
-            <div class="track">
-                <div class="title-track" contenteditable="true" @input="({target})=>track.title=(target as HTMLInputElement)?.innerHTML">
-                    {{ track.title }}
-                </div>
-                <!-- <draggable class="track-items" v-model="track.tasks" group="tasks" item-key="id">
-                    <template #item="{ element }">
-                        <KanbanTask v-bind="element" />
-                    </template>
-                </draggable>
-                <AddTask :id="track.id" /> -->
-                <KanbanTaskList :track-id="track.id"/>
-            </div>
-        </template>
-    </draggable>
-    <BoardActions />
+  <draggable group="track" v-model="tracks" class="tracks" item-key="id">
+    <template #item="{ element: track }">
+      <div class="track">
+        <div class="title-track" contenteditable="true"
+          @input="({ target }) => track.title = (target as HTMLInputElement)?.innerHTML">
+          {{ track.title }}
+        </div>
+        <draggable group="tasks" v-model="track.tasks" item-key="id">
+          <template #item="{ element: task }">
+            <KanbanTask :task="task" />
+          </template>
+        </draggable>
+        <AddTask :id="track.id" />
+      </div>
+    </template>
+  </draggable>
+  <BoardActions />
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
 import BoardActions from './BoardActions.vue';
-import KanbanTaskList from './KanbanTaskList.vue';
+import KanbanTask from './KanbanTask.vue';
+import AddTask from './AddTask.vue';
 import draggable from "vuedraggable";
 import { useKanbanStore } from '../stores/useKanbanStore';
 
 
 const store = useKanbanStore();
 const tracks = ref(store.tracks);
-const drag = ref(false);
 </script>
